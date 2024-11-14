2b59fdb07670d2b50d832d8f0cd519e6acab11d2
PixelatedPaws <paw.rexie@gmail.com>
2024-11-14 16:45:42 -0500

    Added the models for Ingredient, MenuItem, Recipierequirments, and PurchaseLog

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ingredient, MenuItem, RecipieRequirements, PurchaseLog
 
-# Register your models here.
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_per_unit', 'quantity_in_stock')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    
+@admin.register(RecipieRequirements)
+class RecipieRequirementsAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'ingredients')
+    
+@admin.register(PurchaseLog)
+class PurchaseLogAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'quantity_purchased', 'time_purchased')
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_in_stock = models.PositiveIntegerField
+    
+    def __str__(self):
+        return self.name
+    
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+class RecipieRequirements(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+    
+    def __str__(self):
+        ingredient_list = [ingredient.name for ingredient in self.ingredients.all()]
+        return f"{self.menu_item.name} requires: {', '.join(ingredient_list)}"
+    
+class PurchaseLog(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity_purchased = models.PositiveIntegerField
+    time_purchased = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.quantity_purchased} units of {self.menu_item.name} purchased at {self.time_purchased}"
+    
